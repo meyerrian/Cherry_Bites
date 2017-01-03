e49c0ff570ef192e42323717b0b61bf981c2c92a
meyerrian <meyerrian@gmail.com>
2017-01-03 19:03:47 +1100

    added recent posts code and styles to welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @posts = Post.all.order("created_at desc").limit(1)
+    @recents = Post.all.order("created_at desc").offset(1)
   end
 end
