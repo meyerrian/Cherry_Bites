4d00451e568c8094be4a61760131473b4b8549da
meyerrian <meyerrian@gmail.com>
2017-01-04 15:53:40 +1100

    added code for contacts view

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  #sendgrid settings
+  config.action_mailer.default_url_options = {host: 'https://infinite-fjord-44681.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+
   #Sets paperclip to AS3
   config.paperclip_defaults = {
     storage: :s3,
