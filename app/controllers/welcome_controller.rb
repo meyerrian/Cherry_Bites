cb8ca1a0f396bfe16bac5cbe8e92b307018b5d2f
meyerrian <meyerrian@gmail.com>
2017-01-04 11:52:15 +1100

    added code for popular on welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @posts = Post.all.order("created_at desc").limit(1)
     @recents = Post.all.order("created_at desc").offset(1)
+    @popular = Post.all.order("rating desc").limit(4)
   end
 end
